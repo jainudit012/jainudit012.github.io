67340326a6fc0201c96e7dd614ead2e763c446f6
Udit Jain <udit.jain@opscale.io>
2020-03-09 17:30:15 +0530

    add mast paginator; add nav list hover; increase accessiblity score

---
+++ scripts/mastScroller.js
@@ -0,0 +1,63 @@
+console.log('mastScroller.js LOADED\n', '*** REMOVE in PROD ***')
+
+function addClass(element, className){
+    const arrOfClassNames = element.className.split(" ")
+    if(arrOfClassNames.indexOf(className) === -1) element.className += ` ${className}`
+}
+
+function removeClass(element, className){
+    element.className = element.className.replace(className, "")
+}
+
+const startNumber = document.getElementById('start__count')
+const endNumber = document.getElementById('end__count')
+
+const startPaginator = document.getElementById('paginator__progress-start')
+const midPaginator = document.getElementById('paginator__progress-mid')
+const endPaginator = document.getElementById('paginator__progress-end')
+
+const mast1 = document.getElementById('mast-1')
+const mast2 = document.getElementById('mast-2')
+const mast3 = document.getElementById('mast-3')
+
+startPaginator.addEventListener('click', () => {
+    startNumber.innerHTML = "01"
+    removeClass(startNumber, 'paginator__greyed')
+    addClass(endNumber, 'paginator__greyed')
+
+    addClass(startPaginator, 'paginator__current-progress')
+    removeClass(midPaginator, 'paginator__current-progress')
+    removeClass(endPaginator, 'paginator__current-progress')
+
+    removeClass(mast1, 'hideabs')
+    addClass(mast2, 'hideabs')
+    addClass(mast3, 'hideabs')
+})
+
+midPaginator.addEventListener('click', () => {
+    startNumber.innerHTML = "02"
+    removeClass(startNumber, 'paginator__greyed')
+    addClass(endNumber, 'paginator__greyed')
+
+    removeClass(startPaginator, 'paginator__current-progress')
+    addClass(midPaginator, 'paginator__current-progress')
+    removeClass(endPaginator, 'paginator__current-progress')
+
+    addClass(mast1, 'hideabs')
+    removeClass(mast2, 'hideabs')
+    addClass(mast3, 'hideabs')
+})
+
+endPaginator.addEventListener('click', () => {
+    startNumber.innerHTML = "03"
+    removeClass(endNumber, 'paginator__greyed')
+    addClass(startNumber, 'paginator__greyed')
+
+    removeClass(startPaginator, 'paginator__current-progress')
+    removeClass(midPaginator, 'paginator__current-progress')
+    addClass(endPaginator, 'paginator__current-progress')
+    
+    addClass(mast1, 'hideabs')
+    addClass(mast2, 'hideabs')
+    removeClass(mast3, 'hideabs')
+})

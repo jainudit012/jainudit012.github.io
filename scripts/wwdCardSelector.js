14f03a6a5264245106913abe3cbcbe2115cb6df1
Udit Jain <udit.jain@opscale.io>
2020-03-10 14:43:35 +0530

    refactor code; remove declared constants

--- scripts/wwdCardSelector.js
+++ scripts/wwdCardSelector.js
@@ -1,20 +1,25 @@
 const allCards = []
-const numberOfCards = 8
-const defaultSelectedCard = 1
-
-for (let i = 0; i < numberOfCards; i++){
-    const element = document.getElementById(`wwd__card-${i+1}`)
-    allCards.push(element)
-}
-
-allCards.forEach(card => {
-    card.addEventListener('click', ()=>{
-        const selectedCard = parseInt(card.id.split('-')[1])
+try {
+    const wrapper = document.getElementById('wwd__card__box')
+    const numberOfCards = wrapper ? wrapper.children.length : 0
+    if(numberOfCards !== 0){
+        for (let i = 0; i < numberOfCards; i++){
+            const element = document.getElementById(`wwd__card-${i+1}`)
+            allCards.push(element)
+        }
+        
         allCards.forEach(card => {
-            if(card.id.indexOf(selectedCard) === -1) {
-                removeClass(card, 'card__selected')
-            }
+            card.addEventListener('click', ()=>{
+                const selectedCard = parseInt(card.id.split('-')[1])
+                allCards.forEach(card => {
+                    if(card.id.indexOf(selectedCard) === -1) {
+                        removeClass(card, 'card__selected')
+                    }
+                })
+                addClass(card, 'card__selected')
+            })
         })
-        addClass(card, 'card__selected')
-    })
-})
+    }
+}catch(err){
+    console.log(err)
+}

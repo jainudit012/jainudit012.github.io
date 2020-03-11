7d71b6a332a5a3c9b02e88c10204e9bee053c821
Udit Jain <udit.jain@opscale.io>
2020-03-11 17:54:25 +0530

    refactor code

--- scripts/wwdCardSelector.js
+++ scripts/wwdCardSelector.js
@@ -1,24 +1,8 @@
-const allCards = []
 try {
     const wrapper = document.getElementById('wwd__card__box')
-    const numberOfCards = wrapper ? wrapper.children.length : 0
-    if(numberOfCards !== 0){
-        for (let i = 0; i < numberOfCards; i++){
-            const element = document.getElementById(`wwd__card-${i+1}`)
-            allCards.push(element)
-        }
-        
-        allCards.forEach(card => {
-            card.addEventListener('click', ()=>{
-                const selectedCard = parseInt(card.id.split('-')[1])
-                allCards.forEach(card => {
-                    if(card.id.indexOf(selectedCard) === -1) {
-                        removeClass(card, 'card__selected')
-                    }
-                })
-                addClass(card, 'card__selected')
-            })
-        })
+    const allCardData = loadElementsToArray([], 'wwd__card-', wrapper ? wrapper.children.length : 0)
+    if(allCardData.valid){
+        addSelectItemListener(allCardData.items, 'card__selected', false)
     }
 }catch(err){
     console.log(err)

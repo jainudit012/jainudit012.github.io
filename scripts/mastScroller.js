965dcc94553af393b02a4d55110bc18f1dbaadd7
Udit Jain <udit.jain@opscale.io>
2020-03-13 18:10:38 +0530

    refactor mast controller; add auto mast selection

--- scripts/mastScroller.js
+++ scripts/mastScroller.js
@@ -1,46 +1,59 @@
 const startNumber = document.getElementById('start__count')
 const endNumber = document.getElementById('end__count')
 
-const startPaginator = document.getElementById('paginator__progress-start')
-const midPaginator = document.getElementById('paginator__progress-mid')
-const endPaginator = document.getElementById('paginator__progress-end')
+const numMastHeads = 3
 
-const mast1 = document.getElementById('mast-1')
-const mast2 = document.getElementById('mast-2')
-const mast3 = document.getElementById('mast-3')
+const mastHeads = []
+for(i=0;i<numMastHeads;i++){
+    mastHeads.push(document.getElementById(`mast-${i+1}`))
+}
 
-startPaginator.addEventListener('click', () => {
-    startNumber.innerHTML = "01"
-    removeClass(startNumber, 'paginator__greyed')
-    addClass(endNumber, 'paginator__greyed')
+const mastPaginators = []
+for(i=0;i<numMastHeads;i++){
+    mastPaginators.push(document.getElementById(`paginator__progress-${i+1}`))
+}
 
-    addClass(startPaginator, 'paginator__current-progress')
-    removeClassFromMultiple([midPaginator, endPaginator], 'paginator__current-progress')
+let mastSelected
 
-    removeClass(mast1, 'hideabs')
-    addClassToMultiple([mast2, mast3], 'hideabs')
-})
+const mastAutoSelector = () => {
+    switch(mastSelected){
+        case 2:
+            mastPaginators[2].click()
+            break
+        case 3:
+            mastPaginators[0].click()
+            break
+        default:
+            mastPaginators[1].click()
+    }
+}
 
-midPaginator.addEventListener('click', () => {
-    startNumber.innerHTML = "02"
-    removeClass(startNumber, 'paginator__greyed')
-    addClass(endNumber, 'paginator__greyed')
+let mastSelctorTimer = window.setInterval(mastAutoSelector, 7000)
 
-    removeClassFromMultiple([startPaginator, endPaginator], 'paginator__current-progress')
-    addClass(midPaginator, 'paginator__current-progress')
+mastPaginators.forEach(paginator => {
+    const paginatorNum = parseInt(paginator.id.split('-')[1])
 
-    addClassToMultiple([mast1, mast3], 'hideabs')
-    removeClass(mast2, 'hideabs')
-})
-
-endPaginator.addEventListener('click', () => {
-    startNumber.innerHTML = "03"
-    removeClass(endNumber, 'paginator__greyed')
-    addClass(startNumber, 'paginator__greyed')
-
-    removeClassFromMultiple([startPaginator, midPaginator], 'paginator__current-progress')
-    addClass(endPaginator, 'paginator__current-progress')
+    paginator.addEventListener('click', e => {
+        mastSelected = paginatorNum
+        clearInterval(mastSelctorTimer)
+        if(!e.isTrusted){
+            mastSelctorTimer = window.setInterval(mastAutoSelector, 7000)
+        }
     
-    addClassToMultiple([mast1, mast2], 'hideabs')
-    removeClass(mast3, 'hideabs')
+        startNumber.innerHTML = `0${paginatorNum}`
+
+        addClass(mastPaginators[paginatorNum-1], 'paginator__current-progress')
+        removeClassFromMultiple(mastPaginators.filter((_, index)=> index!==(paginatorNum-1)), 'paginator__current-progress')
+
+        removeClass(mastHeads[paginatorNum-1], 'hideabs')
+        addClassToMultiple(mastHeads.filter((_, index)=> index!==(paginatorNum-1)), 'hideabs')
+
+        if(paginatorNum !== mastPaginators.length){
+            removeClass(startNumber, 'paginator__greyed')
+            addClass(endNumber, 'paginator__greyed')
+        }else {
+            removeClass(endNumber, 'paginator__greyed')
+            addClass(startNumber, 'paginator__greyed')
+        }
+    })
 })

172fa5bc18ce00dbb3aba7ee1b493f0a3da59e5d
Udit Jain <udit.jain@opscale.io>
2020-03-24 18:49:37 +0530

    add blog page js

--- scripts/blogPage.js
+++ scripts/blogPage.js
@@ -1,5 +1,18 @@
+const blogSection = document.getElementById('blogs__section')
+
+const optionToggleMenus = []
+
 try{
     const allBlogData = loadElementsToArray('ii__card-', blogWrapper)
+
+    const blogArticleData = loadElementsToArray('blog-', blogArticlesWrapper)
+
+    if(blogArticleData.valid){
+        for(i=0;i<blogArticleData.items.length;i++){
+            optionToggleMenus.push(document.getElementById(`options-blog-${i+1}-1`))
+            optionToggleMenus.push(document.getElementById(`options-blog-${i+1}-2`))
+        }
+    }
 
     function changeHashToQuery(){
         const blogHash = window.location.hash
@@ -13,66 +26,108 @@ try{
         }
         loadBlogsFromQuery()
     }
-
+ 
     function loadBlogsFromQuery(){
         const blogQuery = window.location.search
 
-        switch (blogQuery){
-            case '?type=pm': {
-                filterBlogs('pm', allBlogData.items, 'filtertag')
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'pm') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-                break
+        window.scrollTo(0 , 0)
+
+        if(blogQuery.indexOf('tab') !== -1){
+            let queries = {}
+            blogQuery.replace('?', '').split('&').forEach(query => {
+                queries[query.split('=')[0]] = query.split('=')[1]
+            })
+            loadBlog(queries)
+        }else{
+            addClass(blogArticlesWrapper, 'hidden')
+
+            switch (blogQuery){
+                case '?type=pm': {
+                    filterBlogs('pm', allBlogData.items, 'filtertag')
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'pm') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                    break
+                }
+                case '?type=dev': {
+                    filterBlogs('dev', allBlogData.items, 'filtertag')
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'dev') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                    break
+                }
+                case '?type=per': {
+                    filterBlogs('per', allBlogData.items, 'filtertag')
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'per') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                    break
+                }
+                case '?type=ux': {
+                    filterBlogs('ux', allBlogData.items, 'filtertag')
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'ux') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                    break
+                }
+                case '?type=app': {
+                    filterBlogs('app', allBlogData.items, 'filtertag')
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'app') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                    break
+                }
+                case '?type=sta': {
+                    filterBlogs('sta', allBlogData.items, 'filtertag')
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'sta') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                    break
+                }
+                default: {
+                    filterBlogs('all', allBlogData.items, 'filtertag') 
+                    blogFilterTabData.forEach(tab => {
+                        if(tab.dataset.filtertag === 'all') addClass(tab, 'blogs__nav__item--selected')
+                        else removeClass(tab, 'blogs__nav__item--selected')
+                    })
+                }
             }
-            case '?type=dev': {
-                filterBlogs('dev', allBlogData.items, 'filtertag')
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'dev') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-                break
-            }
-            case '?type=per': {
-                filterBlogs('per', allBlogData.items, 'filtertag')
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'per') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-                break
-            }
-            case '?type=ux': {
-                filterBlogs('ux', allBlogData.items, 'filtertag')
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'ux') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-                break
-            }
-            case '?type=app': {
-                filterBlogs('app', allBlogData.items, 'filtertag')
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'app') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-                break
-            }
-            case '?type=sta': {
-                filterBlogs('sta', allBlogData.items, 'filtertag')
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'sta') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-                break
-            }
-            default: {
-                filterBlogs('all', allBlogData.items, 'filtertag') 
-                blogFilterTabData.forEach(tab => {
-                    if(tab.dataset.filtertag === 'all') addClass(tab, 'blogs__nav__item--selected')
-                    else removeClass(tab, 'blogs__nav__item--selected')
-                })
-            }
+        }
+    }
+
+    function loadBlog(query){
+        removeClassFromMultiple(allBlogData.items, 'hidden')
+
+        removeClass(floatingIcon , 'invisible')
+
+        addClass(allBlogData.items.filter(card => card.id.indexOf(query.selected) !== -1)[0], 'hidden')
+
+        if(query.tab === 'all'){
+            addClassToMultiple(allBlogData.items.filter(card => card.id.indexOf(query.selected) === -1).slice(3), 'hidden')
+        }else {
+            addClassToMultiple(allBlogData.items.filter(card => ((card.id.indexOf(query.selected) === -1) && (card.dataset.filtertag !== query.tab))), 'hidden')
+        }
+
+        removeClass(blogArticlesWrapper, 'hidden')
+        addClass(blogArticlesWrapper, 'block')
+        
+        blogFilterTabData.forEach(tab => {
+            if(tab.dataset.filtertag === query.tab) addClass(tab, 'blogs__nav__item--selected')
+            else removeClass(tab, 'blogs__nav__item--selected')
+        })
+
+        if(blogArticleData.valid){
+            blogArticleData.items.forEach(article => {
+                if(article.id.indexOf(query.selected) !== -1){
+                    removeClass(article, 'hidden')
+                }else addClass(article, 'hidden')
+            })
         }
     }
 
@@ -86,7 +141,6 @@ try{
 
     if(toggleSearchInput){
         searchInputLabel.children[0].addEventListener('click', ()=>{
-            console.log('svg')
             addClassToSvg(searchInputLabel.children[0], 'hide')
             removeClass(searchInputLabel.children[1], 'hideabs')
         })
@@ -109,6 +163,16 @@ try{
             addClass(searchInputLabel.children[1], 'hideabs')
             removeClassFromSvg(searchInputLabel.children[0], 'hide')
         }
+    })
+
+    optionToggleMenus.forEach(menu=> {
+        menu.addEventListener('click', ()=>{
+            if(menu.parentNode.children[0].className.indexOf('options-slideOut') !== -1) {
+                removeClass(menu.parentNode.children[0], 'options-slideOut')
+            }else {
+                addClass(menu.parentNode.children[0], 'options-slideOut')
+            }
+        })
     })
 
 }catch(ex){
